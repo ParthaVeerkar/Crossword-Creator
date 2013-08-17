887eb94c5cf897ae6ec528f984ce8be50c84068d
Partha Veerkar <partha.veerkar@gmail.com>
2013-08-17 20:44:32 +0530

    Searched for matching word

--- run.py
+++ run.py
@@ -20,24 +20,23 @@ class MainHandler(webapp2.RequestHandler):
         data = re.findall('<span.*</span>',wiki)
         words = re.findall(r'<b>.*?</b>&nb', data[0])
         meanings = re.findall(r'100%>.*?<', data[0])
-        '''
-        self.response.out.write(data[0])
-        self.response.out.write(words)
-        '''
+        for i in range(0,len(words)):
+            words[i] = words[i][3:-7]
+            meanings[i] = meanings[i][5:-1]
 
         htmlString = '''
         <!doctype html>
-<html lang="en">
-  <head>
-    <title>JQueries</title>
-    <meta charset="utf-8" />
+        <html lang="en">
+        <head>
+        <title>JQueries</title>
+        <meta charset="utf-8" />
 
-<link rel="stylesheet" type="text/css" href="static/index.css" />
-  </head>
-  <body>
+        <link rel="stylesheet" type="text/css" href="static/index.css" />
+        </head>
+        <body>
 
-<div id="wrapper">
-<table id="tab">
+        <div id="wrapper">
+        <table id="tab">
         <tr>
         <td class="tabdata"><input type="text" class="text" maxlength="1" tabindex="1" /></td>
         <td class="tabdata"><input type="text" class="text" maxlength="1" tabindex="2"/></td>
@@ -48,6 +47,35 @@ class MainHandler(webapp2.RequestHandler):
         </tr>
         '''
 
+        table = []        # contains list of (x, y, letter)
+        word_counter = 1  # current word being searched/appended
+        hints = []        # contains list of (word's index in words[], x, y, across/down). Used to remember where a word starts
+
+        # Adding the first word
+        hints.append([1,0,0,0])        #  [word count, x, y, across/down]
+
+        for i in range(0, len(words[word_counter])):
+            table.append([i,0,words[word_counter][i]])
+
+        word_counter+=1
+        flag = 0
+        for i in range(0,len(words)):
+            for j in range(0,len(words[word_counter])):
+                if words[i].count(words[word_counter][j])>0:
+                    flag = 1
+                    break
+                    index_of_old_word = words[i].index(words[word_counter][j])
+                    index_of_new_word = j
+                    matched_word_counter = i
+                    # match found. Check for placement of word
+
+        if flag==1:
+            self.response.out.write("FOUNDDD")
+            self.response.out.write(words[i].index(words[word_counter][j]))
+
+        self.response.out.write(words[word_counter])
+        self.response.out.write('|')
+        self.response.out.write(words[i])
 
         htmlString = htmlString + '''</table><script type="text/javascript" src="static/index.js"></script></body></html>'''
 
@@ -57,7 +85,7 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render())
         '''
-        self.response.out.write(htmlString)
+        #       self.response.out.write(htmlString)
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
                               debug=True
